5a704a1395fb9588f3409675c30a6470849719d0
KorpayDev <dev@korpay.com>
2026-01-21 14:51:36 +0900

    feat: NextJs 다이나믹 import 제거

--- next-sample/app/auth/components/auth.tsx
+++ next-sample/app/auth/components/auth.tsx
@@ -3,7 +3,7 @@
 import './auth.css';
 
 import { useState } from 'react'
-import {RequestData} from "@korpay/sdk";
+import KorpaySDK, { RequestData } from "@korpay/sdk";
 
 interface Props {
   initData: RequestData
@@ -18,9 +18,6 @@ export default function AuthClient({ initData }: Props) {
       alert("데이터를 확인해 주세요.");
       return;
     }
-
-    /* 클릭 시점에 동적 import */
-    const KorpaySDK = (await import('@korpay/sdk')).default
 
     KorpaySDK.payment("https://BASE_URL", paymentData, {
       onStart: () => {
@@ -41,69 +38,69 @@ export default function AuthClient({ initData }: Props) {
   }
 
   return (
-      <div className="checkout-card">
-        <div className="card-header">
-          <h2>결제 금액</h2>
-          <div className="amount">{paymentData.amount.toLocaleString()}원</div>
-        </div>
-
-        <div className="card-body">
-          <div className="info-row">
-            <span>상품명</span>
-            <span>{paymentData.productName}</span>
-          </div>
-
-          <div className="info-row">
-            <span>주문번호</span>
-            <span>{paymentData.orderNumber}</span>
-          </div>
-
-          {paymentData.customerName && (
-              <div className="info-row">
-                <span>구매자 명</span>
-                <span>{paymentData.customerName}</span>
-              </div>
-          )}
-
-          {paymentData.customerEmail && (
-              <div className="info-row">
-                <span>구매자 이메일</span>
-                <span>{paymentData.customerEmail}</span>
-              </div>
-          )}
-
-          {paymentData.customerPhone && (
-              <div className="info-row">
-                <span>구매자 전화번호</span>
-                <span>{paymentData.customerPhone}</span>
-              </div>
-          )}
-
-          {paymentData.customerAddress && (
-              <div className="info-row">
-                <span>구매자 주소</span>
-                <span>{paymentData.customerAddress}</span>
-              </div>
-          )}
-
-          {paymentData.customerPost && (
-              <div className="info-row">
-                <span>구매자 우편번호</span>
-                <span>{paymentData.customerPost}</span>
-              </div>
-          )}
-
-          <div className="divider"></div>
-
-          <button
-              id="payBtn"
-              className={`pay-btn ${btnDisabled ? "loading" : ""}`}
-              onClick={handlePayment}
-              disabled={btnDisabled}
-          >
-            결제하기
-          </button>
-        </div>
+    <div className="checkout-card">
+      <div className="card-header">
+        <h2>결제 금액</h2>
+        <div className="amount">{paymentData.amount.toLocaleString()}원</div>
       </div>
+
+      <div className="card-body">
+        <div className="info-row">
+          <span>상품명</span>
+          <span>{paymentData.productName}</span>
+        </div>
+
+        <div className="info-row">
+          <span>주문번호</span>
+          <span>{paymentData.orderNumber}</span>
+        </div>
+
+        {paymentData.customerName && (
+          <div className="info-row">
+            <span>구매자 명</span>
+            <span>{paymentData.customerName}</span>
+          </div>
+        )}
+
+        {paymentData.customerEmail && (
+          <div className="info-row">
+            <span>구매자 이메일</span>
+            <span>{paymentData.customerEmail}</span>
+          </div>
+        )}
+
+        {paymentData.customerPhone && (
+          <div className="info-row">
+            <span>구매자 전화번호</span>
+            <span>{paymentData.customerPhone}</span>
+          </div>
+        )}
+
+        {paymentData.customerAddress && (
+          <div className="info-row">
+            <span>구매자 주소</span>
+            <span>{paymentData.customerAddress}</span>
+          </div>
+        )}
+
+        {paymentData.customerPost && (
+          <div className="info-row">
+            <span>구매자 우편번호</span>
+            <span>{paymentData.customerPost}</span>
+          </div>
+        )}
+
+        <div className="divider"></div>
+
+        <button
+          id="payBtn"
+          className={`pay-btn ${btnDisabled ? "loading" : ""}`}
+          onClick={handlePayment}
+          disabled={btnDisabled}
+        >
+          결제하기
+        </button>
+      </div>
+    </div>
   );
 }
