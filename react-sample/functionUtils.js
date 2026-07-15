d3d1c87feed3fb4634ab2815f6c38e4c7328b296
KorpayDev <dev@korpay.com>
2026-07-15 16:46:55 +0900

    refact: 시간 함수 kst 적용

--- react-sample/functionUtils.js
+++ react-sample/functionUtils.js
@@ -1,11 +1,12 @@
 export const getCurrentDateTime = () => {
     const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const kst = new Date(now.getTime() + (now.getTimezoneOffset() * 60000) + 9 * 360000); // KST (UTC+9)
+    const year = kst.getFullYear();
+    const month = (kst.getMonth() + 1).toString().padStart(2, "0");
+    const day = kst.getDate().toString().padStart(2, "0");
+    const hours = kst.getHours().toString().padStart(2, "0");
+    const minutes = kst.getMinutes().toString().padStart(2, "0");
+    const seconds = kst.getSeconds().toString().padStart(2, "0");
 
     return year + month + day + hours + minutes + seconds;
 }
